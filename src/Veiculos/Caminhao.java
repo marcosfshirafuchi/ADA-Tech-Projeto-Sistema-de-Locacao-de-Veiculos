e90f3b83b401a505f88ff4daa59b0b3e23d4f0e0
Claudia <claudiasrib@gmail.com>
2025-02-07 18:36:56 -0300

    Criando a classe CarroComum e Caminhao

---
+++ src/Veiculos/Caminhao.java
@@ -0,0 +1,48 @@
+package Veiculos;
+
+public class Caminhao extends Veiculo {
+    private double capacidadeCarga;
+    private int numeroEixos;
+
+    public Caminhao(int codigoVeiculo, String modelo, String placa, double valorDaDiaria, boolean disponibilidade, double capacidadeCarga, int numeroEixos) {
+        super(codigoVeiculo, modelo, placa, valorDaDiaria, disponibilidade);
+        this.capacidadeCarga = capacidadeCarga;
+        this.numeroEixos = numeroEixos;
+    }
+
+    public double getCapacidadeCarga() {
+        return capacidadeCarga;
+    }
+
+    public void setCapacidadeCarga(double capacidadeCarga) {
+        this.capacidadeCarga = capacidadeCarga;
+    }
+
+    public int getNumeroEixos() {
+        return numeroEixos;
+    }
+
+    public void setNumeroEixos(int numeroEixos) {
+        this.numeroEixos = numeroEixos;
+    }
+}
+//    @Override
+//    public String toString() {
+//        return "Caminhao{" +
+//                "codigoVeiculo=" + getCodigoVeiculo() +
+//                ", modelo='" + getModelo() + '\'' +
+//                ", placa='" + getPlaca() + '\'' +
+//                ", valorDaDiaria=" + getValorDaDiaria() +
+//                ", disponibilidade=" + isDisponibilidade() +
+//                ", capacidadeCarga=" + capacidadeCarga + " toneladas" +
+//                ", numeroEixos=" + numeroEixos +
+//                '}';
+//    }
+//
+//    // Método para calcular o custo do aluguel com taxa adicional para caminhões grandes
+//    public double calcularCustoAluguel(int dias) {
+//        double taxaAdicional = capacidadeCarga > 10 ? 1.15 : 1.0;
+//        return dias * getValorDaDiaria() * taxaAdicional;
+//    }
+//}
+//
---
+++ src/Veiculos/CarroComum.java
@@ -0,0 +1,35 @@
+package Veiculos;
+
+public class CarroComum extends Veiculo {
+    private String tipoCombustivel;
+
+    public CarroComum(int codigoVeiculo, String modelo, String placa, double valorDaDiaria, boolean disponibilidade, String tipoCombustivel) {
+        super(codigoVeiculo, modelo, placa, valorDaDiaria, disponibilidade);
+        this.tipoCombustivel = tipoCombustivel;
+    }
+
+    public String getTipoCombustivel() {
+        return tipoCombustivel;
+    }
+
+    public void setTipoCombustivel(String tipoCombustivel) {
+        this.tipoCombustivel = tipoCombustivel;
+    }
+
+//    @Override
+//    public String toString() {
+//        return "CarroComum{" +
+//                "codigoVeiculo=" + getCodigoVeiculo() +
+//                ", modelo='" + getModelo() + '\'' +
+//                ", placa='" + getPlaca() + '\'' +
+//                ", valorDaDiaria=" + getValorDaDiaria() +
+//                ", disponibilidade=" + isDisponibilidade() +
+//                ", tipoCombustivel='" + tipoCombustivel + '\'' +
+//                '}';
+//    }
+//
+//
+//    public double calcularCustoAluguel(int dias) {
+//        return dias * getValorDaDiaria();
+//    }
+}
