36ca86fc4f283426d6e8e26056b34297a9ca3c11
BrunoFerreira1990 <111455462+BrunoFerreira1990@users.noreply.github.com>
2025-02-09 23:49:31 -0300

    Adicionando veículos na base de dados

--- src/BancoDeDados/RegistrosDosBancosDeDados.java
+++ src/BancoDeDados/RegistrosDosBancosDeDados.java
@@ -17,26 +17,80 @@ public class RegistrosDosBancosDeDados {
     public static List<Veiculo> registroVeiculos() {
         List<Veiculo> listaDeVeiculos = new ArrayList<>();
         listaDeVeiculos.add(new Suv(1, "Honda CRV", "ABC-2719", TipoVeiculo.SUV.getValorDaDiaria(), true, TipoVeiculo.SUV.getTipoDoVeiculo()));
+
         listaDeVeiculos.add(new Suv(2, "Jeep Compass", "ABC-2720", TipoVeiculo.SUV.getValorDaDiaria(), true, TipoVeiculo.SUV.getTipoDoVeiculo()));
+
         listaDeVeiculos.add(new Suv(3, "Hyundai Creta", "ABC-2721", TipoVeiculo.SUV.getValorDaDiaria(), true, TipoVeiculo.SUV.getTipoDoVeiculo()));
+
         listaDeVeiculos.add(new Suv(4, "Volkswagen Nivus", "ABC-2722", TipoVeiculo.SUV.getValorDaDiaria(), false, TipoVeiculo.SUV.getTipoDoVeiculo()));
+
         listaDeVeiculos.add(new Suv(5, "Chevrolet Equinox Premier", "ABC-2723", TipoVeiculo.SUV.getValorDaDiaria(), true, TipoVeiculo.SUV.getTipoDoVeiculo()));
+
         listaDeVeiculos.add(new Suv(6, "Toyota Corolla Cross XR", "ABC-2724", TipoVeiculo.SUV.getValorDaDiaria(), true, TipoVeiculo.SUV.getTipoDoVeiculo()));
 
-        //Exemplo de Carro Comum
         listaDeVeiculos.add(new CarroComum(7, "Gol", "ABC-2725", TipoVeiculo.CARRO_COMUM.getValorDaDiaria(), true, TipoVeiculo.CARRO_COMUM.getTipoDoVeiculo()));
 
-        //Exemplo de Carro Premiun
-        listaDeVeiculos.add(new CarroComum(8, "BMW X1", "ABC-2726", TipoVeiculo.CARRO_PREMIUM.getValorDaDiaria(), true, TipoVeiculo.CARRO_PREMIUM.getTipoDoVeiculo()));
+        listaDeVeiculos.add(new CarroPremium(8, "BMW X1", "ABC-2726", TipoVeiculo.CARRO_PREMIUM.getValorDaDiaria(), true, TipoVeiculo.CARRO_PREMIUM.getTipoDoVeiculo()));
 
-        //Exemplo de SUV
         listaDeVeiculos.add(new Suv(9, "Honda WR-V ", "ABC-2727", TipoVeiculo.SUV.getValorDaDiaria(), true, TipoVeiculo.SUV.getTipoDoVeiculo()));
 
-        //Exemplo de Moto
         listaDeVeiculos.add(new Moto(10, "Yamaha Crosser 150", "ABC-2728", TipoVeiculo.MOTO.getValorDaDiaria(), true, TipoVeiculo.MOTO.getTipoDoVeiculo()));
 
-        //Exemplo de Caminhão
         listaDeVeiculos.add(new Caminhao(11, "Mercedes-Benz Acello 1017", "ABC-2729", TipoVeiculo.CAMINHAO.getValorDaDiaria(), true, TipoVeiculo.CAMINHAO.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new CarroComum(12, "Fiat Mobi", "ABC-2730", TipoVeiculo.CARRO_COMUM.getValorDaDiaria(), false, TipoVeiculo.CARRO_COMUM.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new CarroComum(13, "Fiat Argo", "ABC-2731", TipoVeiculo.CARRO_COMUM.getValorDaDiaria(), true, TipoVeiculo.CARRO_COMUM.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new CarroComum(14, "Fiat Palio", "ABC-2732", TipoVeiculo.CARRO_COMUM.getValorDaDiaria(), false, TipoVeiculo.CARRO_COMUM.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new CarroComum(15, "Fiat Stylo", "ABC-2733", TipoVeiculo.CARRO_COMUM.getValorDaDiaria(), true, TipoVeiculo.CARRO_COMUM.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new CarroComum(16, "Fiat Uno", "ABC-2734", TipoVeiculo.CARRO_COMUM.getValorDaDiaria(), true, TipoVeiculo.CARRO_COMUM.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new CarroComum(17, "Fiat Doblo", "ABC-2735", TipoVeiculo.CARRO_COMUM.getValorDaDiaria(), false, TipoVeiculo.CARRO_COMUM.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new CarroComum(18, "Fiat Punto", "ABC-2736", TipoVeiculo.CARRO_COMUM.getValorDaDiaria(), false, TipoVeiculo.CARRO_COMUM.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new CarroComum(19, "Volkswagen Polo", "ABC-2737", TipoVeiculo.CARRO_COMUM.getValorDaDiaria(), true, TipoVeiculo.CARRO_COMUM.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new CarroComum(20, "Volkswagen Up", "ABC-2738", TipoVeiculo.CARRO_COMUM.getValorDaDiaria(), true, TipoVeiculo.CARRO_COMUM.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new CarroComum(21, "Volkswagen Up", "ABC-2738", TipoVeiculo.CARRO_COMUM.getValorDaDiaria(), true, TipoVeiculo.CARRO_COMUM.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new Caminhao(22, "Mercedes-Benz Atego 2426", "ABC-2739", TipoVeiculo.CAMINHAO.getValorDaDiaria(), true, TipoVeiculo.CAMINHAO.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new Caminhao(23, "Mercedes-Benz Actros 2651", "ABC-2740", TipoVeiculo.CAMINHAO.getValorDaDiaria(), false, TipoVeiculo.CAMINHAO.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new Caminhao(24, "Volvo FH 540", "ABC-2741", TipoVeiculo.CAMINHAO.getValorDaDiaria(), true, TipoVeiculo.CAMINHAO.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new Caminhao(25, "Volvo FH 540", "ABC-2742", TipoVeiculo.CAMINHAO.getValorDaDiaria(), false, TipoVeiculo.CAMINHAO.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new Caminhao(26, "Scania R 450", "ABC-2743", TipoVeiculo.CAMINHAO.getValorDaDiaria(), true, TipoVeiculo.CAMINHAO.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new Moto(27, "Honda CG 160", "ABC-2744", TipoVeiculo.MOTO.getValorDaDiaria(), false, TipoVeiculo.MOTO.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new Moto(28, "Honda XRE 300", "ABC-2745", TipoVeiculo.MOTO.getValorDaDiaria(), true, TipoVeiculo.MOTO.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new Moto(29, "Yamaha MT-03", "ABC-2746", TipoVeiculo.MOTO.getValorDaDiaria(), false, TipoVeiculo.MOTO.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new Moto(30, "BMW G 310 GS", "ABC-2747", TipoVeiculo.MOTO.getValorDaDiaria(), true, TipoVeiculo.MOTO.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new Moto(31, "Kawasaki Z400", "ABC-2748", TipoVeiculo.MOTO.getValorDaDiaria(), false, TipoVeiculo.MOTO.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new Moto(32, "Suzuki V-Strom 650", "ABC-2749", TipoVeiculo.MOTO.getValorDaDiaria(), true, TipoVeiculo.MOTO.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new CarroPremium(33, "Audi Q3", "ABC-2751", TipoVeiculo.CARRO_PREMIUM.getValorDaDiaria(), false, TipoVeiculo.CARRO_PREMIUM.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new CarroPremium(34, "Mercedes-Benz GLA 200", "ABC-2752", TipoVeiculo.CARRO_PREMIUM.getValorDaDiaria(), true, TipoVeiculo.CARRO_PREMIUM.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new CarroPremium(35, "Volvo XC60", "ABC-2753", TipoVeiculo.CARRO_PREMIUM.getValorDaDiaria(), false, TipoVeiculo.CARRO_PREMIUM.getTipoDoVeiculo()));
+        listaDeVeiculos.add(new CarroPremium(36, "Lexus NX 350h", "ABC-2754", TipoVeiculo.CARRO_PREMIUM.getValorDaDiaria(), true, TipoVeiculo.CARRO_PREMIUM.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new CarroPremium(37, "Porsche Macan", "ABC-2755", TipoVeiculo.CARRO_PREMIUM.getValorDaDiaria(), false, TipoVeiculo.CARRO_PREMIUM.getTipoDoVeiculo()));
+
+        listaDeVeiculos.add(new CarroPremium(38, "Jaguar F-PACE", "ABC-2756", TipoVeiculo.CARRO_PREMIUM.getValorDaDiaria(), true, TipoVeiculo.CARRO_PREMIUM.getTipoDoVeiculo()));
+
 
         return listaDeVeiculos;
     }
